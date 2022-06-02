5bdac0f9a36b0bc042e16edea51e49655b3a01d0
MrEsteban1 <estebangonz98@@gmail.com>
2022-06-02 18:30:08 -0300

    Desafio 6 parte de productos mejorado

--- desafio-6/public/index.js
+++ desafio-6/public/index.js
@@ -1,27 +1,52 @@
-const socket = io()
-    // const input = document.getElementById('chat-message')
-const button = document.querySelector('#form-subir')
-     console.log('cargado...')
+const socket = io();
+const button = document.querySelector("#form-subir");
 
- window.onload = ()=>{
-     console.log('hola')
-    socket.emit('nueva conexion', 'Se conecto otra persona')
- }
+const input = document.querySelector("#input-mensaje");
+const button_send = document.querySelector("#mensaje-section");
 
-socket.on("productos", async data => {
-    console.log("pasa productos", data)
-    let html =''
-    if(data.length > 0){
-        await data.forEach( dato => {
-            html += `<tr>
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Se toco boton producto.");
+  const datosForm = document.querySelectorAll("form input");
+
+  const producto = {
+    title: datosForm[0].value,
+    price: datosForm[1].value,
+    thumbnail: datosForm[2].value,
+  };
+
+  console.log("productos", producto);
+
+  fetch("http://localhost:8080/productos", {
+    method: "POST", // or 'PUT'
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(producto), // data can be `string` or {object}!
+  });
+});
+
+button_send.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Mensaje:" + input.value);
+});
+
+window.onload = () => {
+  console.log("hola");
+  socket.emit("nueva conexion", "Se conecto otra persona");
+};
+
+socket.on("productos", async (data) => {
+  console.log("pasa productos", data);
+  let html = "";
+  if (data.length > 0) {
+    await data.forEach((dato) => {
+      html += `<tr>
             <th>${dato.id}</th>
             <th>${dato.title}</th>
             <th>${dato.price}</th>
             <th><img src="${dato.thumbnail}" width="100"/></th>
-        </tr>`
-        });
-    } 
+        </tr>`;
+    });
+  }
 
-    document.querySelector("#products-table").innerHTML = html
-})
-
+  document.querySelector("#products-table").innerHTML = html;
+});
--- desafio-6/server.js
+++ desafio-6/server.js
@@ -1,65 +1,55 @@
-const express = require('express')
-const { Server: IOServer } = require('socket.io')
-const { Server: HttpServer} = require('http')
+const express = require("express");
+const { Server: IOServer } = require("socket.io");
+const { Server: HttpServer } = require("http");
 
-const app = express()
+const app = express();
 
-const PORT = 8080
+const PORT = 8080;
 
 let productos = [
-    {
-        title: "Pañal",
-        price: 150,
-        thumbnail: "http://static1.abc.es/Media/201405/19/panal--644x362.jpg",
-        id: 0,
-    }, 
-]
+  {
+    title: "Pañal",
+    price: 150,
+    thumbnail: "http://static1.abc.es/Media/201405/19/panal--644x362.jpg",
+    id: 0,
+  },
+];
 
 let mensajes = [
-    {
-        usuario: "cabildo@gmail.com",
-        mensaje: "Buenas tardes, que tal?"
-    }
-]
+  {
+    usuario: "cabildo@gmail.com",
+    mensaje: "Buenas tardes, que tal?",
+  },
+];
 
-const httpServer = new HttpServer(app)
-const io = new IOServer(httpServer)
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
 
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.sendFile('public/index.html',{root:__dirname})
-})
+app.get("/", (req, res) => {
+  res.sendFile("public/index.html", { root: __dirname });
+});
 
-app.post("/productos", (req, res)=>{
-    req.body
-    productos = [...productos,{id:productos.length,...req.body}]
-    console.log("Datos de productos: ", productos)
-    io.emit("productos", productos)
-})
+app.post("/productos", (req, res) => {
+  productos = [...productos, { id: productos.length, ...req.body }];
+  console.log("Datos de productos: ", req.body);
+  io.emit("productos", productos);
+});
 
-app.post("/mensaje",(req,res)=>{
-    
-})
+app.post("/mensaje", (req, res) => {});
 
-httpServer.listen(PORT,()=> {
-    console.log('Servidor funcionando en puerto ' + PORT)
-})
+httpServer.listen(PORT, () => {
+  console.log("Servidor funcionando en puerto " + PORT);
+});
 
-io.on('connection', async (socket) => {
-    socket.emit("productos", productos)
-    console.log('Usuario conectado ' + socket.id)
-    // io.emit('mensaje bienvenida', 'Bienvenido al chat')
-    // io.emit("mensajes", mensajes)
-    // io.on("agregar producto", data => {
-    //     io.emit("nuevo producto", data)
-    // })
-    socket.on("nueva conexion", () =>{
-        console.log('pasa por nueva conexion')
-        io.emit("productos", productos)
-    })
-})
-
-
+io.on("connection", async (socket) => {
+  socket.emit("productos", productos);
+  console.log("Usuario conectado " + socket.id);
+  socket.on("nueva conexion", () => {
+    console.log("pasa por nueva conexion");
+    io.emit("productos", productos);
+  });
+});
