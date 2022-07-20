7814f202c4e74d53cbfa1cfbb68ab9151c837acb
Esteban Gonzalez <estebangonz98@gmail.com>
2022-07-20 10:30:09 -0300

    actualizacion back

--- desafio-11/public/home/index.js
+++ desafio-11/public/home/index.js
@@ -1,13 +1,8 @@
-const e = require("express")
-
 const boton = document.getElementById('return-button')
 boton.addEventListener('click', (e)=>{
     e.preventDefault()
     
-    fetch('http://localhost:8080/logout',{
-        method: GET,
-        headers: { "Content-type": "application/json" },
-    }).then(_ =>window.location.href = 'http://localhost:8080/login' )
+    fetch('http://localhost:8080/logout').then(_ =>window.location.href = 'http://localhost:8080/login' )
 
     
 })
